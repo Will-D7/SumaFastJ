90a5980f749c6fd81994e9b915e57cf7b6bae4e2
Will-D7 <williamgonza777@gmail.com>
2022-07-22 00:51:15 -0400

    doc: v1 Suma rapida, pendiente de añadir mas operaciones o cosas raras.

---
+++ SumaFast.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class SumaFast{
+	private static int var1;
+	private static int var2;
+	private String str1;
+	private static Scanner scan1 = new Scanner(System.in);
+	private static Scanner scan2 = new Scanner(System.in);
+	private static int operation1;
+
+	public static void main(String args[]){
+		System.out.println("Introduzca el primer número...");
+		var1 = scan1.nextInt();
+		
+		System.out.println("Introduzca el segundo número...");
+		var2 = scan2.nextInt();
+		
+		operation1 = suma(var1,var2);
+		
+		System.out.println("Resultado de la suma es:  "+operation1);
+	}
+	public static int suma(int a, int b){
+		return a+b;
+	}
+}
